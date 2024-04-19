e8c8eee43481fb40c517de56102d468874f93454
Hannah Mox <moxhannah04@gmail.com>
2024-04-18 22:41:28 -0400

    Clean up
    Fixed string variables, function new line, and f at the end of float literals

--- MojoCode.py
+++ MojoCode.py
@@ -1,9 +1,14 @@
 if __name__ == '__main__':
 
-	a = 1
-	b = a + 2
-
-	if(a == b):
+	num1 = 3
+	num2 = 4
+	if(num1 == num2):
 		print("hi jordan")
+
 	else:
-		print("please")
+		print("hi hannah")
+	hello = "hello world"
+	num3 = 3.99
+	num4 = 4
+	while(num3 == num4):
+		print("bye")

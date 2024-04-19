d7136ea3925fd95528ca0969d07c2e855487fac4
Hannah Mox <moxhannah04@gmail.com>
2024-04-18 23:51:10 -0400

    Fixed small multiplication issue

--- MojoCode.py
+++ MojoCode.py
@@ -1,14 +1,6 @@
 if __name__ == '__main__':
 
-	num1 = 3
-	num2 = 4
+	num1 = 6
+	num2 = 4.0
 	if(num1 == num2):
 		print("hi jordan")
-
-	else:
-		print("hi hannah")
-	hello = "hello world"
-	num3 = 3.99
-	num4 = 4
-	while(num3 == num4):
-		print("bye")
